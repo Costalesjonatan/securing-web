a34352e271020988cc1c51b9259c6a139e436d37
Jonatan <costalesjonatan@gmail.com>
2024-08-05 15:52:44 -0300

    Added basic login, login error and logout pages, basic security login configuration.

---
+++ src/main/java/com/example/securing_web/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.securing_web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Value("${user.property}")
+    private String userProperty;
+
+    @Value("${password.property}")
+    private String passwordProperty;
+
+    @Value("${role.property}")
+    private String roleProperty;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username(userProperty)
+                        .password(passwordProperty)
+                        .roles(roleProperty)
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
