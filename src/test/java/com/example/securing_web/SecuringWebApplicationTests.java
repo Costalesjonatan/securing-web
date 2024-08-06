a9e6f0bdb300ceaac1ec9712866c00f450b2ba93
Jonatan <costalesjonatan@gmail.com>
2024-08-06 11:41:28 -0300

    Added basic unit test, remove unused imports.

--- src/test/java/com/example/securing_web/SecuringWebApplicationTests.java
+++ src/test/java/com/example/securing_web/SecuringWebApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.securing_web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.BDDAssertions.then;
 
 @SpringBootTest
 class SecuringWebApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		then(context).isNotNull();
 	}
 
 }
